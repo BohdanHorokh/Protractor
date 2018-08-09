6cc1d84f3d281a133df50d8d8ebb474de2d56ecf
Bogdan Gorokh <bogdan.gorokh@eleks.com>
2018-08-09 17:40:15 +0300

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,7 +11,6 @@ pipeline {
         }
         stage('Test') {
             steps {
-                sh 'webdriver-manager start'
                 sh 'protractor ./configs/conf.js'
             }
         }
--- configs/conf.js
+++ configs/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     // directConnect: true,
     framework: 'jasmine2',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: 'http://localhost:3000',
     specs: ['../specs/testAllure.js'],
     // specs: ['../specs/testMatchers.js'],
     suites: {
